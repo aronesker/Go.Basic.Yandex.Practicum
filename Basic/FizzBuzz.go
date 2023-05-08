4484e049e61de82917701be5c4bbcd7bd8a44ddd
Pavel Pshenichnikov <31566218+aronesker@users.noreply.github.com>
2023-05-08 14:17:37 +0600

    Add files via upload
    repaired

--- Basic/FizzBuzz.go
+++ Basic/FizzBuzz.go
@@ -4,16 +4,16 @@ import "fmt"
 
 func main() {
 	for i := 1; i <= 100; i++ {
+		if i%15 == 0 {
+			fmt.Println("FizzBuzz")
+			continue
+		}
 		if i%3 == 0 {
 			fmt.Println("Fizz")
 			continue
 		}
 		if i%5 == 0 {
 			fmt.Println("Buzz")
-			continue
-		}
-		if i%15 == 0 {
-			fmt.Println("FizzBuzz")
 			continue
 		}
 		fmt.Println(i)
