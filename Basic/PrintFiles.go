2caf7fd8a273597f7ccfe64236d8b679245b9fae
Pavel Pshenichnikov <31566218+aronesker@users.noreply.github.com>
2023-05-11 15:53:49 +0600

    Add files via upload

---
+++ Basic/PrintFiles.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	PrintAllFilesWithFilter(".", "slices")
+}
+
+func PrintAllFilesWithFilter(path string, filter string) {
+	// получаем список всех элементов в папке (и файлов, и директорий)
+	files, err := os.ReadDir(path)
+	if err != nil {
+		fmt.Println("unable to get list of files", err)
+		return
+	}
+	//  проходим по списку
+	for _, f := range files {
+		// получаем имя элемента
+		// filepath.Join — функция, которая собирает путь к элементу с разделителями
+		filename := filepath.Join(path, f.Name())
+		// печатаем имя элемента, если путь к нему содержит filter
+		if strings.Contains(filename, filter) {
+			fmt.Println(filename)
+		}
+		// если элемент — директория, то вызываем для него рекурсивно ту же функцию
+		if f.IsDir() {
+			PrintAllFilesWithFilter(filename, filter)
+		}
+	}
+}
